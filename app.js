b92ce5b4a613694fbb3311915b0bfed56cb34c1b
Stephane Moreau <st.moreau@outlook.com.gr>
2017-04-24 14:58:48 +0100

    configured routing for work-enter and work-track

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var work_track = require('./routes/work-track');
+var work_enter = require('./routes/work-enter');
 
 var app = express();
 
@@ -22,8 +22,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/work', work_track);
+app.use('/', work_enter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
